32ce99cab35af160d46cf82a12709776e50ad2ee
José Honorio de Oliveira Sobrinho Neto <honorio.dev@gmail.com>
2026-02-03 16:03:39 -0300

    refactor: move `auth.module.ts` for `/api/v1/controllers/auth`

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,7 @@ import { ConfigModule } from '@nestjs/config';
 import configuration from './configuration';
 import { JwtConfigModule } from './infra/auth/jwt/jwt.config.module';
 import { ValidationModule } from './infra/validation/zod/validation.module';
-import { AuthModule } from './domain/auth/auth.module';
+import { AuthModule } from './api/v1/controllers/auth/auth.module';
 
 @Module({
   imports: [
